66ab1f2d52066f01e443f6dbfa2d4d4ba7112e03
meloknaasfalte <you@example.com>
2023-01-07 13:37:42 +0300

    feat: spa

---
+++ online-store/src/core/templates/components.ts
@@ -0,0 +1,15 @@
+abstract class Component {
+    protected container: HTMLElement;
+
+    constructor (tagName: string, className: string, content: string) {
+        this.container = document.createElement(tagName);
+        this.container.className = className;
+        this.container.innerHTML = content;
+    }
+
+    render() {
+        return this.container;
+    }
+}
+
+export default Component;
